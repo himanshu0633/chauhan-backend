dc59c00fe303c4e45f06eb938128f91057e69ac2
Nikhil Kumar <nik326990@gmail.com>
2025-09-17 17:57:30 +0530

    chane

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
